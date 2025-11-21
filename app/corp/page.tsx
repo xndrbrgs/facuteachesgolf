a5ce31b449f226ef736521da75ca66856959526d
xndrbrgs <aborgescolon@gmail.com>
2025-11-20 22:11:41 -0500

    image gallery incoming

--- app/corp/page.tsx
+++ app/corp/page.tsx
@@ -2,6 +2,7 @@
 
 import CircleButton from "@/components/anims/Circle";
 import CorpHero from "@/components/hero/CorpHero";
+import WhoAmI from "@/components/hero/WhoAmI";
 import Lenis from "lenis";
 import { useEffect } from "react";
 
@@ -19,11 +20,12 @@ export default function CorpPage() {
     requestAnimationFrame(raf);
   }, []);
   return (
-    <main className="relative overflow-x-hidden bg-background">
-      <div className="fixed bottom-[clamp(120px,160px)] right-[clamp(40px,160px)] z-60">
+    <main className="relative overflow-x-hidden bg-background text-white px-[clamp(16px,40px)]">
+      <div className="fixed bottom-[clamp(120px,160px)] right-[clamp(40px,160px)] z-90">
         <CircleButton text="BOOK NOW!" />
       </div>
       <CorpHero />
+      <WhoAmI />
     </main>
   );
 }
---
+++ components/hero/WhoAmI.tsx
@@ -0,0 +1,62 @@
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+import { useRef } from "react";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger, SplitText);
+
+const WhoAmI = () => {
+  const container = useRef(null);
+
+  useGSAP(() => {
+    // Split text into words
+    const split = new SplitText("#span", { type: "words" });
+
+    // Animate each word's opacity with stagger tied to scroll
+    gsap.from(split.words, {
+      opacity: 0,
+      color: "#999",
+      y: 20,
+      stagger: {
+        each: 0.05,
+        ease: "power1.out",
+      },
+      scrollTrigger: {
+        trigger: container.current,
+        start: "top 60%",
+        end: "bottom bottom",
+        scrub: true,
+      },
+    });
+  }, []);
+
+  return (
+    <section className="relative my-[clamp(80px,120px)] w-full h-[300vh]">
+      <div
+        className="sticky left-0 top-0 h-screen flex flex-col justify-center"
+        ref={container}
+      >
+        <h1 className="h1">WHO AM I</h1>
+        <div className="py-[clamp(60px,96px)] flex flex-col gap-y-[clamp(24px,32px)]">
+          <span className="h4" id="span">
+            I help companies and executives create world-class golf
+            experiences—drawing on years of inside-the-ropes expertise from the
+            PGA Tour, LPGA Tour, and Korn Ferry Tour.
+          </span>
+          <span className="h4" id="span">
+            From tailored golf schools to premium corporate events, I bring the
+            precision, strategy, and professionalism of tour-level golf to your
+            business gatherings.
+          </span>
+        </div>
+      </div>
+      <div className="h-[200vh] w-full">
+        <div>IMAGE GALLERY</div>
+      </div>
+    </section>
+  );
+};
+
+export default WhoAmI;
