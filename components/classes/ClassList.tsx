5ba00e70b73ad543e5e55149ed95177ad19b064b
Alex Borges <aborgescolon@gmail.com>
2025-11-30 17:15:59 -0500

    Changing deps

--- components/classes/ClassList.tsx
+++ components/classes/ClassList.tsx
@@ -1,7 +1,7 @@
 const ClassList = () => {
   return (
     <section className="h-screen bg-[#ecece9] text-black px-[clamp(16px,40px)] pt-[clamp(56px,96px)]">
-      <h1 className="h1">ClassList</h1>
+      <h1 className="h1">ClassList Test</h1>
     </section>
   );
 };
