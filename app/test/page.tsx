dabe6cedb14f0f5ebcb367f8ca86fdf08dffe12d
xndrbrgs <aborgescolon@gmail.com>
2026-08-19 15:19:42 -0400

    now

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -10,7 +10,7 @@ import PlainCircle from "@/components/anims/PlainCircle";
 const LandingPage = () => {
   return (
     <main className="relative overflow-x-hidden bg-background text-white w-full">
-      <div className="fixed bottom-[clamp(120px,160px)] right-[clamp(40px,80px)] z-80">
+      <div className="fixed bottom-[clamp(60px,160px)] right-[clamp(40px,80px)] z-80">
          <PlainCircle text="BOOK CLASS" />
       </div>
       <LandingHero />
